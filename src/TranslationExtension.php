b02e26ab4832b9a54b08b809dacfaec58b1dd4f3
Raphael Castro <rafaphcastro@gmail.com>
2016-03-15 10:57:11 -0300

    First commit

---
+++ src/TranslationExtension.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @author Raphael Castro <rafaphcastro@gmail.com>
+ * @copyright 2016 Raphael Castro
+ * @package Raph\Twig\Extension
+ * 
+ */
+namespace Raph\Twig\Extension;
+
+class Translation extends \Twig_Extension
+{
+  private $translator = NULL;
+
+  public function __construct($translator) {
+    $this->translator = $translator;
+  }
+
+  public function getName()
+  {
+    return 'translate';
+  }
+
+  public function getFunctions()
+  {
+    return [
+      new \Twig_SimpleFunction('translate', [$this, 'translate']),
+      new \Twig_SimpleFunction('_', [$this, 'translate'])
+    ];
+  }
+
+  public function translate($name)
+  { 
+    $translator = $this->translator;
+    if(!$translator) {
+      throw new \Exception('No translator class found.');
+    }
+    if(!method_exists($translator, 'trans')) {
+      throw new \Exception('No translate method found in translator class.');
+    }    
+    return $translator->trans($name);
+  }
+}
